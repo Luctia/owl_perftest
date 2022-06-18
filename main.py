633403b1e2626f51a68a44af9982dd724e19e684
Luctia <timmerman.luc1999@gmail.com>
2022-06-18 14:26:01 +0200

    WIP

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+TYPES = ["long", "deep", "stacked", "declaration", "mixed"]
+
+
 def progress_bar(progress, total):
     percent = 100 * (progress / float(total))
     bar = '█' * int(percent) + '-' * int(100 - percent)
@@ -26,7 +29,6 @@ def generate_grammars(N, step_size=50):
 
 
 def run_generating_tests():
-    types = ["long", "deep", "stacked", "declaration", "mixed"]
     files = os.listdir("tests")
     for type in ["deep"]:
         print("Running generating tests for " + type + " grammars...")
@@ -58,7 +60,8 @@ def run_generating_tests():
 
 def generate_graph(data, grammar_type):
     x = np.linspace(0, int(max([int(key) for key in data.keys()])), int(len(data)))
-    y = [time['time'] for time in data.values()]
+    time_to_generate = [info['time'] for info in data.values()]
+    lines = [info['lines'] for info in data.values()]
 
     # fit a linear curve an estimate its y-values and their error.
     # a, b = np.polyfit(x, y, deg=1)
@@ -71,12 +74,15 @@ def generate_graph(data, grammar_type):
     # ax.fill_between(x, y_est - y_err, y_est + y_err, alpha=0.2)
     # ax.plot(x, y, 'o', color='tab:brown')
 
-    fig, ax = plt.subplots()
-    ax.plot(x, y)
+    fig, ax1 = plt.subplots()
+    ax1.plot(x, time_to_generate)
 
-    ax.set(xlabel='length of grammar', ylabel='time (s)',
+    ax1.set(xlabel='length of grammar', ylabel='time (s)',
            title='Time to generate a parser for a ' + grammar_type + ' grammar of a certain length')
-    ax.grid()
+    ax1.grid()
+
+    ax2 = ax1.twinx()
+    ax2.plot(x, lines)
 
     plt.savefig(grammar_type + '.png')
 
@@ -94,6 +100,19 @@ def generate_graphs():
 
 def add_line_counts():
     print("Couting lines...")
+    files = os.listdir(os.getcwd() + "/parsers/")
+    for type in ["deep"]:
+        type_files = [file for file in files if file.__contains__(type)]
+        current_data = json.load(open(os.getcwd() + "/" + type + "_results.json"))
+        for file in type_files:
+            with open(os.getcwd() + "/parsers/" + file, "r") as parser:
+                # Read all lines that aren't comments or empty
+                content = parser.read()
+                lines = len(re.findall(r"\n[^/\n]", content))
+                parser.close()
+            current_data[file.split('_')[1][:-2]]['lines'] = lines
+        json.dump({int(entry): current_data[entry] for entry in current_data.keys()}, open(os.getcwd() + "/" + type + "_results.json", "w"), sort_keys=True, indent=4)
+    print("Done.")
 
 
 if __name__ == '__main__':
@@ -101,6 +120,7 @@ if __name__ == '__main__':
         os.mkdir("tests")
     if not os.path.isdir(os.getcwd() + "/parsers/"):
         os.mkdir("parsers")
-    generate_grammars(5, step_size=20)
+    generate_grammars(200, step_size=10)
     run_generating_tests()
+    add_line_counts()
     generate_graphs()
